a3f66ac25593ee7cc96fc59fd8c3fa5c8c2aa210
Invisible-Media <invisible-media@outlook.com>
2019-02-14 11:14:13 +0100

    untracked files added

---
+++ Electron4/electron.cmd
@@ -0,0 +1,8 @@
+@IF EXIST "%~dp0\node.exe" (
+  "%~dp0\node.exe"  "%~dp0\node_modules\electron\cli.js" %*
+) ELSE (
+  @SETLOCAL
+  @SET PATHEXT=%PATHEXT:;.JS;=;%
+  node  "%~dp0\node_modules\electron\cli.js" %*
+)
+PAUSE
